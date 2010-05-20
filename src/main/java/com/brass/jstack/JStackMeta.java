0217c7c58f97314e7607a3486adb3df4b2e65c72
Will Dampney <willdampney@trip.uni.newcastle.edu.au>
2010-05-20 12:11:30 +1000

    JStackMeta and testcase

--- src/main/java/com/brass/jstack/JStackMeta.java
+++ src/main/java/com/brass/jstack/JStackMeta.java
@@ -1,5 +1,6 @@
 package com.brass.jstack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,5 +10,27 @@ public class JStackMeta {
     private StringBuilder _header;
     private List<JStackEntry> _entries;
 
-    
+    public JStackMeta() {
+        _header = new StringBuilder(4);
+        _entries = new ArrayList<JStackEntry>();
+    }
+
+    public StringBuilder getHeader() {
+        return _header;
+    }
+
+    public List<JStackEntry> getEntries() {
+        return _entries;
+    }
+
+    public void append(final String line) {
+        _header.append(line);
+    }
+
+    public void addEntry(final JStackEntry jStackEntry) {
+        if (jStackEntry == null || jStackEntry.getContents().length() == 0) {
+            return;
+        }
+        _entries.add(jStackEntry);
+    }
 }
---
+++ test/main/java/com/brass/jstack/JStackMetaTest.java
@@ -0,0 +1,47 @@
+package com.brass.jstack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author willdampney
+ */
+public class JStackMetaTest {
+    @Test
+    public void testConstructorInit() throws Exception {
+        Assert.assertNotNull(new JStackMeta().getHeader());
+        Assert.assertNotNull(new JStackMeta().getEntries()); 
+    }
+
+    @Test
+    public void testAppend() throws Exception {
+        final JStackMeta stackMeta = new JStackMeta();
+        stackMeta.append("Test Meta");
+        Assert.assertEquals(9, stackMeta.getHeader().length());
+    }
+
+    @Test
+    public void testAddNullEntry() throws Exception {
+        final JStackMeta stackMeta = new JStackMeta();
+        stackMeta.addEntry(null);
+        Assert.assertEquals(0, stackMeta.getEntries().size());
+    }
+
+
+    @Test
+    public void testAddEmptyEntry() throws Exception {
+        final JStackMeta stackMeta = new JStackMeta();
+        stackMeta.addEntry(new JStackEntry());
+        Assert.assertEquals(0, stackMeta.getEntries().size());
+    }
+
+
+    @Test
+    public void testAddEntry() throws Exception {
+        final JStackMeta stackMeta = new JStackMeta();
+        final JStackEntry stackEntry = new JStackEntry();
+        stackEntry.append("Test Entry");
+        stackMeta.addEntry(stackEntry);
+        Assert.assertEquals(1, stackMeta.getEntries().size());
+    }
+}
